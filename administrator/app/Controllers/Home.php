0bfbceb32d211d812cfa71e7a4f98c5026a3863c
TKF-Takahashi <tkf5.osaka.naoya@gmail.com>
2025-08-27 14:36:13 +0900

    エラーに対処(#0014)

--- administrator/app/Controllers/Home.php
+++ administrator/app/Controllers/Home.php
@@ -3,7 +3,10 @@
 namespace App\Controllers;
 
 class Home extends BaseController
-{
+{	public function __construct()
+	{
+		helper('url'); // この行を追加
+	}
     public function index(): string
     {
         echo 'test';
