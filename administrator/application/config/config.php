9edab9d85d00d0ddadd4956a13323de15b964126
TKF-Takahashi <tkf5.osaka.naoya@gmail.com>
2025-08-31 23:22:31 +0900

    エラー対応(#0021)

--- administrator/application/config/config.php
+++ administrator/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://13.236.153.2/administrator/';
+$config['base_url'] = 'http://13.236.153.2/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://13.236.153.2/administrator/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
