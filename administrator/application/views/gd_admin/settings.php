7b9c7902f4ed595d04920705c03c1a71dc86089a
TKF-Takahashi <tkf5.osaka.naoya@gmail.com>
2025-09-02 16:48:15 +0900

    更新漏れ(#0025)

--- administrator/application/views/gd_admin/settings.php
+++ administrator/application/views/gd_admin/settings.php
@@ -9,16 +9,16 @@
 
 	<?php echo form_open('gd_admin/settings'); ?>
 		<div class="form-group">
+			<label for="channel_id">チャンネルID</label>
+			<input type="text" class="form-control" name="channel_id" id="channel_id" value="<?php echo html_escape($channel_id); ?>">
+		</div>
+		<div class="form-group">
 			<label for="mentor_role_id">メンターロールID</label>
 			<input type="text" class="form-control" name="mentor_role_id" id="mentor_role_id" value="<?php echo html_escape($mentor_role_id); ?>">
 		</div>
 		<div class="form-group">
 			<label for="admin_role_id">管理者ロールID</label>
 			<input type="text" class="form-control" name="admin_role_id" id="admin_role_id" value="<?php echo html_escape($admin_role_id); ?>">
-		</div>
-		<div class="form-group">
-			<label for="channel_id">チャンネルID</label>
-			<input type="text" class="form-control" name="channel_id" id="channel_id" value="<?php echo html_escape($channel_id); ?>">
 		</div>
 		<button type="submit" class="btn btn-primary">保存</button>
 	</form>
